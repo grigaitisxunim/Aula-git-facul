b4e5c6bdab3e882fa8dfdeda2a41e107da78ab22
grigaitisxunim <junior.grigaitis98@hotmail.com>
2021-08-24 21:36:23 -0300

    segundo

---
+++ empregada.java
@@ -0,0 +1,17 @@
+public class Empregado{
+private String nome;
+private int idade;
+public String getNome (){
+public int filhos;
+return this.nome;
+}
+public void setNome (String nome){
+this.nome = nome;
+}
+public int getIdade (){
+return this.idade;
+}
+public void setIdade (int idade){
+this.idade = idade;
+}
+}

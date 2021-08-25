5681c3c102896569d9157b0891f0994f9eab9013
grigaitisxunim <junior.grigaitis98@hotmail.com>
2021-08-24 21:38:39 -0300

    terceiro...

--- empregada.java
+++ empregada.java
@@ -1,8 +1,26 @@
 public class Empregado{
 private String nome;
 private int idade;
+private int tipo;
+private double salario;
+private double comissao;
+private double bonus;
+public double calculaSalario (){
+if (tipo == 1){
+return salario;
+}
+else if (tipo == 2){
+//comissão varia de 0 a 1, é um percentual sobre o salário
+return salario + salario * comissao;
+}
+else if (tipo == 3){
+return salario + bonus;
+}
+else{
+return 0.;
+}
+}
 public String getNome (){
-public int filhos;
 return this.nome;
 }
 public void setNome (String nome){
